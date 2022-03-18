edb2fb5669b1a491b49ea9b4fcb56a40796a5fde
DmitryVasylchuk <overclocker777@gmail.com>
2022-03-18 14:08:14 +0200

    feat: add short url form

--- resources/views/shorturls/fields.blade.php
+++ resources/views/shorturls/fields.blade.php
@@ -3,15 +3,3 @@
     {!! Form::label('original_url', 'Original Url:') !!}
     {!! Form::textarea('original_url', null, ['class' => 'form-control']) !!}
 </div>
-
-<!-- Short Url Field -->
-<div class="form-group col-sm-12 col-lg-12">
-    {!! Form::label('short_url', 'Short Url:') !!}
-    {!! Form::textarea('short_url', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Redirect Count Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('redirect_count', 'Redirect Count:') !!}
-    {!! Form::number('redirect_count', null, ['class' => 'form-control']) !!}
-</div>
